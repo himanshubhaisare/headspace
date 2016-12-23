f38843a375c835948affe5a67cd79213b6b24118
Himanshu Bhaisare <himanshu.bhaisare@gmail.com>
2016-12-22 23:07:45 -0800

    remove useless error consts

--- src/constants/Error.java
+++ src/constants/Error.java
@@ -4,8 +4,6 @@ public class Error {
 
     public static final String COMMAND_NOT_RECOGNIZED = "\nERROR: command not recognized\n\n> ";
     public static final String INVALID_ARGS = "\nERROR: invalid arguments\n\n> ";
-    public static final String AUTHOR_ALREADY_HAS_BOOK = "\nERROR: this user already has a valid credit card\n\n> ";
-    public static final String TITLE_BELONGS_TO_ANOTHER_AUTHOR = "\nERROR: This title belongs to another author\n\n> ";
     public static final String AUTHOR_NOT_FOUND = "\nERROR: Author not found\n\n> ";
     public static final String TITLE_NOT_FOUND = "\nERROR: Title not found\n\n> ";
 	public static final String UNDO_FAILED = "\nERROR: Cannot undo";
